614130dfd41bd46514a7f98da011fa7b4d075e69
aayusrsta <69640693+aayusrsta@users.noreply.github.com>
2020-09-07 18:28:29 +0545

    Add files via upload

--- js.js
+++ js.js
@@ -73,3 +73,22 @@ function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
   document.getElementById("openNav").style.display = "inline-block";
 }
+
+
+/*3D*/
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
